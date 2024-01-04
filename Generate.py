708a44c8604b11ece99e188a6f6dc6aa472d2193
57U <uu57uu@yandex.com>
2024-01-04 18:37:20 +0800

    update framework

---
+++ Generate.py
@@ -0,0 +1,15 @@
+import os
+
+os.makedirs("out",exist_ok=True)
+f=open("out/out.md","w",encoding="utf-8")
+
+dirs = [i for i in os.listdir() if not os.path.isfile(i) and i not in [".git","out"]]
+for chapter in dirs:
+    f.write("# "+chapter+"\n\n")
+    files=os.listdir(chapter)
+    for file in files:
+        f.write("## "+file.replace(".md","")+"\n")
+        with open(chapter+"/"+file,"r",encoding="utf-8") as f2:
+            f.write(f2.read()) 
+
+f.close()
