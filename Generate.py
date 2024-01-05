11198d8b0c2c9285014082d13511ba5e077e60a2
57U <uu57uu@yandex.com>
2024-01-05 20:16:11 +0800

    update

--- Generate.py
+++ Generate.py
@@ -4,9 +4,11 @@ os.makedirs("out",exist_ok=True)
 f=open("out/index.md","w",encoding="utf-8")
 
 dirs = [i for i in os.listdir() if not os.path.isfile(i) and i not in [".git","out",".github"]]
+dirs.sort()
 for chapter in dirs:
     f.write("# "+chapter+"\n\n")
     files=os.listdir(chapter)
+    files.sort()
     for file in files:
         f.write("## "+file.replace(".md","")+"\n")
         with open(chapter+"/"+file,"r",encoding="utf-8") as f2:
