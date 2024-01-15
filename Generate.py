c92339de0aed66b6d8eac1da05143754d4674807
57U <uu57uu@yandex.com>
2024-01-15 19:35:51 +0800

    update framework

--- Generate.py
+++ Generate.py
@@ -3,15 +3,15 @@ import os
 os.makedirs("out",exist_ok=True)
 f=open("out/index.md","w",encoding="utf-8")
 
-dirs = [i for i in os.listdir() if not os.path.isfile(i) and i not in [".git","out",".github"]]
+dirs = [i for i in os.listdir("src") if not os.path.isfile(i) and i not in [".git","out",".github"]]
 dirs.sort()
 for chapter in dirs:
     f.write("# "+chapter+"\n\n")
-    files=os.listdir(chapter)
+    files=os.listdir("src/"+chapter)
     files.sort()
     for file in files:
         f.write("## "+file.replace(".md","")+"\n")
-        with open(chapter+"/"+file,"r",encoding="utf-8") as f2:
+        with open("src/"+chapter+"/"+file,"r",encoding="utf-8") as f2:
             f.write(f2.read()) 
             f.write("\n\n")
 with open("footer.md") as footer:
